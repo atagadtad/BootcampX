4e9ce8e4f2f0c888616c2754909f0f9c592e7669
atagadtad <aarontagadtad@gmail.com>
2019-09-18 21:31:24 +0000

    sanitized teachers.js and students.js to resist sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,22 +1,26 @@
 const { Pool } = require('pg');
-const args = process.argv.slice(2);
+
 
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
-  database: 'vagrant'
+  database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`${cohortName}`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
